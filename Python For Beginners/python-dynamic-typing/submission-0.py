49e9553e90ada59055bd26ed19a88c6923191b26
lantenak <103672596+lantenak@users.noreply.github.com>
2026-07-01 13:03:04 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 2
+print(type(variable))
+variable = 2.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = '2'
+print(type(variable))
+variable = [2, 2.0, False, '2']
+print(type(variable))
