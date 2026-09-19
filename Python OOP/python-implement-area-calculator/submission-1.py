5a5472f540693f25617860788508bd6791a8f1a6
lantenak <103672596+lantenak@users.noreply.github.com>
2026-09-19 09:17:33 +0300

    Add: python-implement-area-calculator - submission-1

---
+++ Python OOP/python-implement-area-calculator/submission-1.py
@@ -0,0 +1,15 @@
+import math
+
+
+class AreaCalc:
+    def calculate(self, *args: int) -> [float, int]:
+        if len(args) == 1:
+            return round(math.pi * args[0] ** 2, 2)
+        elif len(args) == 2:
+            return args[0] * args[1]
+
+    
+# Don't modify the following code
+calc = AreaCalc()
+print(calc.calculate(5))    
+print(calc.calculate(4, 6))
